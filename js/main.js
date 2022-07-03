21288ac6ca1b274e585e7f0322a1c9ed55ec2051
mehediemon007 <mehediemon7@gmail.com>
2022-07-03 16:36:57 +0600

    footer

--- js/main.js
+++ js/main.js
@@ -6,6 +6,14 @@
         $(".banner-content h1, .banner-content p, .banner-content a").addClass("animate");
     });
 
+    $(".partner-slider").owlCarousel({
+        items: 7,
+        loop: true,
+        autoplay: true,
+        dots: false,
+        margin: 50
+    })
+
     var owl = $(".client-reviews").owlCarousel({
         loop:true,
         autoplay:true,
@@ -35,24 +43,16 @@
 
     owl.on('changed.owl.carousel', function(event) {
 
-        $(".owl-carousel .owl-item").each(function( index, value ) {
+        $(".client-reviews .owl-item").each(function( index, value ) {
             if($(value).hasClass("middle")){
                 $(value).removeClass("middle")
             }
         });
 
-        activeElement = $('.owl-carousel .owl-item.active');
+        activeElement = $('.client-reviews .owl-item.active');
         $(activeElement[1]).addClass("middle")
 
     });
-
-    $(".partner-slider").owlCarousel({
-        items: 7,
-        loop: true,
-        autoplay: true, 
-        dots: false,
-        margin: 50
-    })
 
 })(jQuery)
   
